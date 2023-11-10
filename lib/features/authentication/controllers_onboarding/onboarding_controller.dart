6e67d79882a00bdb2abfa0919342d6347cc3339a
CharanBot <tangellacharan003@gmail.com>
2023-11-10 17:55:42 +0530

    created home and nav bar

--- lib/features/authentication/controllers_onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers_onboarding/onboarding_controller.dart
@@ -2,34 +2,38 @@ import 'package:flutter/cupertino.dart';
 import 'package:food/features/authentication/screens/login/login.dart';
 import 'package:get/get.dart';
 
-class OnBoardingController extends GetxController {
+class OnBoardingController extends GetxController{
   static OnBoardingController get instance => Get.find();
+
 
   final pageController = PageController();
   Rx<int> currentPageIndex = 0.obs;
+  
 
-  ///Update Current Index when page Scroll
-  void updatePageIndicator(index) => currentPageIndex.value = index;
+ ///Update Current Index when page Scroll
+  void updatePageIndicator(index) =>currentPageIndex.value = index ;
 
-  ///Jump to the specific dor selected page
-  void dotNavigationClick(index) {
+///Jump to the specific dor selected page
+   void dotNavigationClick(index){
     currentPageIndex.value = index;
     pageController.jumpTo(index);
-  }
+   }
 
-  ///Update Current Index and Jump to next Page
-  void nextPage() {
-    if (currentPageIndex.value == 2) {
+   ///Update Current Index and Jump to next Page
+   void nextPage() {
+    if(currentPageIndex.value == 2){
       Get.offAll(LoginScreen());
-    } else {
+    }
+    else{
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
     }
-  }
+   }
 
-  ///Update Current Index and Jump to the last Page
-  void skipPage() {
-    currentPageIndex.value = 2;
-    pageController.jumpTo(2);
-  }
+   ///Update Current Index and Jump to the last Page
+   void skipPage() {
+    Get.offAll(const LoginScreen());
+    //currentPageIndex.value = 2;
+    //pageController.jumpTo(2);
+   }
 }
