80abe3ca6cf4ac12d48eb0f230463d8e3118642a
YuanziX <achubadyal4@gmail.com>
2024-04-28 15:04:04 +0530

    pass proper parameters

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -14,9 +14,6 @@
 // import 'package:food/utils/constants/text_strings.dart';
 // //import 'package:food/utils/device/device_utility.dart';
 
-
-
-
 // class OnBoardingScreen extends StatelessWidget {
 //   const OnBoardingScreen
 // ({super.key});
@@ -24,7 +21,6 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     final controller = Get.put(OnBoardingController());
-    
 
 //     return Scaffold(
 //       body: Stack(
@@ -39,7 +35,7 @@
 //                 title: TTexts.onBoardingTitle1,
 //                 subTitle: TTexts.onBoardingSubTitle1,
 //                 ),
-//                 OnBoardingPage( 
+//                 OnBoardingPage(
 //                 image: TImages.onBoardingImage2,
 //                 title: TTexts.onBoardingTitle2,
 //                 subTitle: TTexts.onBoardingSubTitle2,
@@ -51,14 +47,13 @@
 //                 ),
 //             ],
 //           ),
-        
+
 //          ///const  OnBoardingSkip(),
-        
+
 //         const OnBoardingDotNavigation(),
 
 //         const OnBoardingNextButton()
-        
-        
+
 //         ]
 //      ),
 //     );
@@ -75,48 +70,44 @@ import 'package:lottie/lottie.dart';
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
     final data = [
-    CardPlanetData(
-      title: "observe",
-      subtitle:
-          "The night sky has much to offer to those who seek its mystery.",
-      //image: const AssetImage("assets/images/img-1.png"),
-      backgroundColor: const Color.fromRGBO(0, 10, 56, 1),
-      titleColor: Colors.pink,
-      subtitleColor: Colors.white,
-      background: LottieBuilder.asset("assets/images/animations/bg-1.json"),
-    ),
-    CardPlanetData(
-      title: "imagine",
-      subtitle: "An endless number of galaxies means endless possibilities.",
-      //image: const AssetImage("assets/images/img-2.png"),
-      backgroundColor: Colors.white,
-      titleColor: Colors.purple,
-      subtitleColor: const Color.fromRGBO(0, 10, 56, 1),
-      background: LottieBuilder.asset("assets/images/animations/bg-2.json"),
-    ),
-    CardPlanetData(
-      title: "stargaze",
-      subtitle: "The sky dome is a beautiful graveyard of stars.",
-      //image: const AssetImage("assets/images/img-3.png"),
-      backgroundColor: const Color.fromRGBO(71, 59, 117, 1),
-      titleColor: Colors.yellow,
-      subtitleColor: Colors.white,
-      background: LottieBuilder.asset("assets/images/animations/bg-3.json"),
-    ),
-  ];
+      CardPlanetData(
+        title: "observe",
+        subtitle:
+            "The night sky has much to offer to those who seek its mystery.",
+        //image: const AssetImage("assets/images/img-1.png"),
+        backgroundColor: const Color.fromRGBO(0, 10, 56, 1),
+        titleColor: Colors.pink,
+        subtitleColor: Colors.white,
+        background: LottieBuilder.asset("assets/images/animations/bg-1.json"),
+      ),
+      CardPlanetData(
+        title: "imagine",
+        subtitle: "An endless number of galaxies means endless possibilities.",
+        //image: const AssetImage("assets/images/img-2.png"),
+        backgroundColor: Colors.white,
+        titleColor: Colors.purple,
+        subtitleColor: const Color.fromRGBO(0, 10, 56, 1),
+        background: LottieBuilder.asset("assets/images/animations/bg-2.json"),
+      ),
+      CardPlanetData(
+        title: "stargaze",
+        subtitle: "The sky dome is a beautiful graveyard of stars.",
+        //image: const AssetImage("assets/images/img-3.png"),
+        backgroundColor: const Color.fromRGBO(71, 59, 117, 1),
+        titleColor: Colors.yellow,
+        subtitleColor: Colors.white,
+        background: LottieBuilder.asset("assets/images/animations/bg-3.json"),
+      ),
+    ];
     final controller = Get.put(OnBoardingController());
     return Scaffold(
       body: ConcentricPageView(
         colors: data.map((e) => e.backgroundColor).toList(),
         itemCount: data.length,
-        itemBuilder: (int index, double value) {
-          return CardPlanet(data: data[index]);
-        },
+        itemBuilder: (int index) => CardPlanet(data: data[index]),
         onFinish: () {
           controller.skipPage();
           // Navigator.pushReplacement(
@@ -128,12 +119,3 @@ class OnBoardingScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
