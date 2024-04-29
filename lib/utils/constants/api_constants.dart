dc590f715fb4a846709f30380195dfe924170719
YuanziX <achubadyal4@gmail.com>
2024-04-29 18:33:53 +0530

    constants: change db link

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -3,4 +3,4 @@
 // Example
 const String tSecretAPIKey = "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
 
-const String dbLink = 'https://vendor-panel-green.vercel.app/api';
+const String dbLink = 'https://vendor-panel-three.vercel.app/api';
