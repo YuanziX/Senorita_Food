f0635a0b725ff6f70da3471459fa46e96eaaf9d9
YuanziX <achubadyal4@gmail.com>
2024-04-30 23:37:06 +0530

    cart: fix image fill

--- lib/common/widgets/products/cart/cart_item.dart
+++ lib/common/widgets/products/cart/cart_item.dart
@@ -1,36 +1,34 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:food/common/widgets/images/t_rounded_images.dart';
 import 'package:food/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
 import 'package:food/features/shop/models/cart_item_model.dart';
-import 'package:food/utils/constants/colors.dart';
+import 'package:food/features/shop/screens/store/widgets/shimmer_image.dart';
 //import 'package:food/utils/constants/image_strings.dart';
 import 'package:food/utils/constants/sizes.dart';
-import 'package:food/utils/helpers/helper_functions.dart';
 
 import '../../texts/product_title_text.dart';
 
 class TCartItem extends StatelessWidget {
   const TCartItem({
-    super.key, required this.cartItem,
+    super.key,
+    required this.cartItem,
   });
 
-   final CartItemModel cartItem;
-  
+  final CartItemModel cartItem;
+
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      ///Image
-      TRoundedImage(
-        imageUrl: cartItem.image ?? '',
-        width: 60,
-        height: 60,
-        isNetworkImage: true,
-        padding: const EdgeInsets.all(TSizes.sm),
-        backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
+    return Row(
+      children: [
+        ///Image
+        ClipOval(
+          child: ShimmerImage(
+            imageUrl: cartItem.image ?? '',
+            width: 60,
+            height: 60,
+          ),
         ),
         const SizedBox(width: TSizes.spaceBtwItems),
+
         ///Title,Price and Size
         Expanded(
           child: Column(
@@ -38,21 +36,33 @@ class TCartItem extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TBrandTitleWithVerifiedIcon(title: cartItem.brandName ?? ''),
-              Flexible(child: TProductTitleText(title: cartItem.title ,maxLines: 1)),
+              Flexible(
+                  child: TProductTitleText(title: cartItem.title, maxLines: 1)),
+
               ///Attributes
               Text.rich(
                 TextSpan(
-                  children: (cartItem.selectedVariation ?? {}).entries.map(
-                    (e) => TextSpan(
-                      children: [
-                        TextSpan(text: ' ${e.key} ', style: Theme.of(context).textTheme.bodySmall),
-                        TextSpan(text: ' ${e.value} ', style: Theme.of(context).textTheme.bodyLarge),
-                      ],),).toList(),
+                  children: (cartItem.selectedVariation ?? {})
+                      .entries
+                      .map(
+                        (e) => TextSpan(
+                          children: [
+                            TextSpan(
+                                text: ' ${e.key} ',
+                                style: Theme.of(context).textTheme.bodySmall),
+                            TextSpan(
+                                text: ' ${e.value} ',
+                                style: Theme.of(context).textTheme.bodyLarge),
+                          ],
+                        ),
+                      )
+                      .toList(),
                 ),
               )
             ],
           ),
         )
-    ],);
+      ],
+    );
   }
 }
